e72427f930c4470995305f4175c90de1d1d045ef
kretgit <kret86@yandex.ru>
2025-01-21 19:42:56 +0300

    HW-04: заготовки для crud под домашку кубер + БД

---
+++ common/src/main/java/ru/otus/ms/common/exception/CommonExceptionHandler.java
@@ -0,0 +1,17 @@
+package ru.otus.ms.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class CommonExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<?> handleException(CommonException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
